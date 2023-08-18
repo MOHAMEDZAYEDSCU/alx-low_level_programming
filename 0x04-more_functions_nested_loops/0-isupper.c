ee1d0d28c862fe264d01046e5fe483222a710c40
mohamed zayed <mohamedzayedgaber111.com>
2023-08-18 22:17:02 +0300

    task1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 /*#include "main.h"*/
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * main - check the code.
  *
  * Description: check if upper or lower
  *
- * Return: 0 or 1 as needed
+ * Return: 0 Always
  */
 
 int main(void)
@@ -14,8 +15,9 @@ int main(void)
 	char c;
 
 	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, (isupper(c) - 255));
 	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, isupper(c));
+
 	return (0);
 }
