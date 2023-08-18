5c567d13b2d44ef0f79288761db56e6b3452ffdf
mohamed zayed <mohamedzayedgaber111.com>
2023-08-18 23:20:49 +0300

    task4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - to print number from 0 to 9
+ * Description: no thing elseto write
+ * Return: 0 Always
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
