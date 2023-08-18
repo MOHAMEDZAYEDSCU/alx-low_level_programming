d984b6540e6f321e33aa76ab5710fc349c74f9fa
mohamed zayed <mohamedzayedgaber111.com>
2023-08-18 23:39:14 +0300

    task5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - to print number from 0 to 9
+ * Description: no thing elseto write
+ * Return: no return for void function
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		if (i != 2 && i != 4)
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+}
