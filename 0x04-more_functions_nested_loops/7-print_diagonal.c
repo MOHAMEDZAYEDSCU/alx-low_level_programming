59312136958052f2ed0ba464b5d4754c189b8ca5
mohamed zayed <mohamedzayedgaber111.com>
2023-08-19 11:47:11 +0300

    Task8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ * Description: program to print '_' diagonlaly
+ * @n: number of _ you want to print
+ * Return: no return for htis function.
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i, j, x = 1;
+
+	if (n < 1)
+	{
+		putchar('\n');
+		x = 0;
+	}
+	else
+	{
+		for (i = 0 ; i < n ; j++)
+		{
+			for (j = 0 ; i < j ; i++)
+			putchar(' ');
+			putchar('\\');
+		}
+	}
+	if (x)
+	putchar('\n');
+
+}
