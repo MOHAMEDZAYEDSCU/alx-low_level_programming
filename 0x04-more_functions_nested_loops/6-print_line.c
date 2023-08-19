f0827cf06d87b05994177385d7f243e2d69ef937
mohamed zayed <mohamedzayedgaber111.com>
2023-08-19 11:33:50 +0300

    Task7

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,20 +4,23 @@
  * print_line - check the code
  * Description: program to print '_'
  * @n: number of _ you want to print
- * Return: Always 0.
+ * Return: no return for htis function.
+ *
  */
 
 void print_line(int n)
 {
-	int i;
+	int i, x = 1;
 
 	if (n < 1)
 	{
 		putchar('\n');
-		return (0);
+		x = 0;
 	}
 	else
 	for (i = 0 ; i < n ; i++)
 	putchar('_');
+	
+	if (x)
 	putchar('\n');
 }
