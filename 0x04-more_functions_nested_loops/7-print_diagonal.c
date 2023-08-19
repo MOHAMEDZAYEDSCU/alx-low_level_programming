dcb84e93f51e00a9a9b6bc4173c8f63fab710728
mohamed zayed <mohamedzayedgaber111.com>
2023-08-19 12:00:57 +0300

    Task8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -21,8 +21,9 @@ void print_diagonal(int n)
 	{
 		for (i = 0 ; i < n ; i++)
 		{
-			for (j = 0 ; i < j ; j++)
+			for (j = 0 ; j < i ; j++)
 			putchar(' ');
+
 			putchar('\\');
 			putchar('\n');
 		}
