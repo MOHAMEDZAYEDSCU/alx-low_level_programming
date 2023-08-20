ae15941662f70e56f9519ea72918555ddec7173a
mohamed zayed <mohamedzayedgaber111.com>
2023-08-21 00:32:40 +0300

    after_deadline

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is main funciton
+ *
+ * Description: function to print small alphabet
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int x = 26, i;
+
+	for (i = 0 ; i < x ; i++)
+	{
+		putchar((122 - i));
+	}
+	putchar('\n');
+
+	return (0);
+}
