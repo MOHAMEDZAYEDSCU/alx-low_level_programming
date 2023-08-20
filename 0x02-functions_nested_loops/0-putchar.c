c05543e9276dc4c365e685241227f977770125ce
mohamed zayed <mohamedzayedgaber111.com>
2023-08-21 02:20:00 +0300

    after deadline

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - this is main funciton
+ *
+ * Description: for print putchar
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
