8e3321b1ebde55503b0209a1f4d8aceab9c14b64
mohamed zayed <mohamedzayedgaber111.com>
2023-08-21 00:16:35 +0300

    after deadline

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - this is main funciton
+ *
+ * Description: function to print small alphabet
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int x = 26, i;
+	/*char c[] = {'a'};*/
+
+	for (i = 0 ; i < x ; i++)
+	{
+		putchar((97 + i));
+	}
+	for (i = 0 ; i < x ; i++)
+	{
+		putchar((65 + i));
+	}
+	putchar('\n');
+
+	return (0);
+}
