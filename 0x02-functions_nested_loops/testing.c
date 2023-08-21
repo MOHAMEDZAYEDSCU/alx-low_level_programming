1805927892a18d53c96d5afbfb55dbdf52b6ace6
mohamed zayed <mohamedzayedgaber111.com>
2023-08-21 10:05:04 +0300

    after deadline

--- 0x02-functions_nested_loops/testing.c
+++ 0x02-functions_nested_loops/testing.c
@@ -11,18 +11,18 @@ int main(void)
 	int r;
 
 	r = _isalpha('H');
-	_putchar(r + '0');
+	putchar(r + '0');
 
 	r = _isalpha('o');
 	putchar(r + '0');
 
 	r = _isalpha(108);
-	_putchar(r + '0');
+	putchar(r + '0');
 
 	r = _isalpha(108);
-	_putchar(r + '0');
+	putchar(r + '0');
 
-	_putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
