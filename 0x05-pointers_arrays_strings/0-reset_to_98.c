30801a99e754645e891155bb71a9f88a40c9abf7
mohamed zayed <mohamedzayedgaber111.com>
2023-08-21 13:28:21 +0300

    task1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this is main funciton
+ *
+ * Description: reset the value to 98
+ * @n: the value will be reseted
+ *
+ * Return: no return for void funciton
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
