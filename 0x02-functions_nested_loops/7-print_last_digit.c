945e2ca9dff0d70762fe813443b541ac9ca37451
mohamed zayed <mohamedzayedgaber111.com>
2023-08-21 11:34:11 +0300

    after deadline

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,16 +4,18 @@
  * print_last_digit - this is the only funciton
  *
  * Description: printing lowercase characters
- *@n: an integer added
+ *
+ * @n: an integer added
+ *
  * Return: no return for void funciton
  */
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		n = -n;
-	}
+	/*if (n < 0)*/
+	/*{*/
+		/*n = -n;*/
+	/*}*/
 
 	return (n % 10);
 }
