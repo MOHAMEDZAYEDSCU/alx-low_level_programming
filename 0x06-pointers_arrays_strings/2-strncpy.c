19b5f5af858301fe354f17eb956469f59ad6916f
mohamed zayed <mohamedzayedgaber111.com>
2023-08-25 20:02:27 +0300

    task3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - a pointer to a function
+ *
+ * Description: for concatinater 2 strings
+ * @dest: the first input string
+ * @src: the second input string
+ * @n: number of character wanted
+ *
+ * Return: concatinated string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int s2;
+
+	/*while (dest[s1])*/
+		/*s1++;*/
+
+	for (s2 = 0; s2 < n && src[s2] != '\0'; s2++)
+	{
+		dest[s2] = src[s2];
+	}
+
+	return (dest);
+}
