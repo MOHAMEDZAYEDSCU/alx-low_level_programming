5e07c52eae576e5be9a53db3cc6386a87ee004a0
mohamed zayed <mohamedzayedgaber111.com>
2023-08-28 14:25:10 +0300

    task2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,9 +3,9 @@
 /**
  * *_memcpy - funciton to return the address of an array
  *
- * @s: the first address of the array
+ * @dest: the coppied array
+ * @src: the source array
  * @n: the number of elements
- * @b: the element of index i
  *
  * Return: pointer to an array &S
  */
