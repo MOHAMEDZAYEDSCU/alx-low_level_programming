205d04e3c2dd5f9c3d284f1ecfe2fcfe3970790e
mohamed zayed <mohamedzayedgaber111.com>
2023-08-30 12:14:00 +0300

    3

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - funciton to print strings
+ *
+ * Description: no more words to say
+ * @s: pointer to an arary
+ *
+ * Return: no return for void function
+ */
+
+int _strlen_recursion(char *s)
+{
+	int x = strlen(s);
+
+	return (x);
+}
