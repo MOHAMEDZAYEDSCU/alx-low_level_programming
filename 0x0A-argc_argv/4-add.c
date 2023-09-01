65b69b1fa11cf7db995f1b86bf0d44466c5fca18
mohamed zayed <mohamedzayedgaber111.com>
2023-09-02 01:42:29 +0300

    5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,14 +19,12 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c ; c++)
-		{
 			if (*c < '0' || *c > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 			sum += atoi(argv[argc]);
-		}
 	}
 	printf("%d\n", sum);
 	return (0);
