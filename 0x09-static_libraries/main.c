6991bdc05019c7881618de897f23429b8b931fdb
mohamed zayed <mohamedzayedgaber111.com>
2023-09-01 17:40:18 +0300

    1

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - this is main function
+ *
+ * Description: nothing to say
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
