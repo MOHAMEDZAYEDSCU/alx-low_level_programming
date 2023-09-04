92df18b742d63afb943a3c483a98592de6f56696
mohamed zayed <mohamedzayedgaber111.com>
2023-09-04 20:54:33 +0300

    task3

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * *str_concat - this is main function
+ *
+ * @s1: the first pointer
+ * @s2: to the first location in memory
+ *
+ * Return: NULL or pointer to the array.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *x;
+	int i1, i2, i, j;
+
+	if (!s1 && !s2)
+		return (NULL);
+
+	for (i1 = 0 ; s1[i1] != '\0' ; i1++)
+		;
+	for (i2 = 0 ; s2[i2] != '\0' ; i2++)
+		;
+
+	x = malloc(sizeof(char) * (i1 + i2 + 1));
+
+	if (!x)
+		return (NULL);
+
+	for (i = 0 ; i < i1 ; i++)
+		x[i] = s1[i];
+
+	for (j = i ; j < (i1 + i2 + 1) ; j++)
+	{
+		x[j] = *s2;
+		s2++;
+	}
+
+	x[j] = '\0';
+
+	return (x);
+}
