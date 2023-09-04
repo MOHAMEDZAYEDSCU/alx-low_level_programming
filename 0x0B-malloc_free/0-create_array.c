8967902321572449e9d7240d94de56bd2f182d70
mohamed zayed <mohamedzayedgaber111.com>
2023-09-04 18:36:04 +0300

    task1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,21 +6,18 @@
  * @size: the size of variable
  * @c: to the first location in memory
  *
- * Return: NULL or 1
+ * Return: NULL or pointer to the array.
  */
 
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
 	char *x = malloc(size);
 
 	if (size == 0 || x == 0)
-		return (NULL);
+		return (0);
 
-	for (i = 0 ; i < size ; i++)
-	{
-		x[i] = c;
-	}
+	while (size--)
+		x[size] = c;
 
 	return (0);
 }
