73ddf5488016730d7c85b6a78735f7ebcab16948
mohamed zayed <mohamedzayedgaber111.com>
2023-09-07 17:08:01 +0300

    adv1

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _realloc - make block of memory for use
+ *
+ * @ptr: pointer to an array
+ * @old_size: the old array size
+ * @new_size: the new array size
+ *
+ * Return: void function
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	char *res;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	else if (new_size == old_size)
+		return (ptr);
+
+	else if (ptr == NULL)
+	{
+		res = malloc(new_size);
+		if (res == NULL)
+			return (NULL);
+
+		return (res);
+	}
+
+	else
+	{
+		res = realloc(ptr, new_size);
+
+		if (res == NULL)
+			return (NULL);
+
+		for (i = 0 ; i < old_size && i < new_size ; ++i)
+			res[i] = ((char *)ptr)[i];
+	}
+
+	free(ptr);
+	return (ptr);
+}
