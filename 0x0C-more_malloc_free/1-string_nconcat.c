4cce1982cbaf20aeacfbe933f6c35c1cb5f3bc6e
mohamed zayed <mohamedzayedgaber111.com>
2023-09-07 15:35:35 +0300

    2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,12 +18,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-                s2 = "";
+		s2 = "";
 
 	for (s1_len = 0 ; s1[s1_len] ; s1_len++)
 		;
 	for (s2_len = 0 ; s2[s2_len] ; s2_len++)
-                ;
+		;
 
 	temp = (s2_len <= n) ? s2_len : n;
 
