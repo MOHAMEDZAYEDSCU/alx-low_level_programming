14bbc0d5b6a7f63eafb8dae80e8acdf99d0d707b
mohamed zayed <mohamedzayedgaber111.com>
2023-09-11 16:28:03 +0300

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - structure data for dogs
+ *
+ * @name: the name of dog
+ * @age: the age of dog
+ * @owner: the name of owner
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
