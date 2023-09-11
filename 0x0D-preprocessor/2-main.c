77b748394edc18151b5827bf8b9139201303597f
mohamed zayed <mohamedzayedgaber111.com>
2023-09-11 13:56:11 +0300

    task3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Description: typing the name of the file
+ *
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
