c67e2e546896473f272140ce60c6e5cdc23f8677
mohamed zayed <mohamedzayedgaber111.com>
2023-09-13 18:02:14 +0300

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+/**
+ * print_name - function that takes pointer to array of
+ * char and pointer to function
+ *
+ * @name: i explain before
+ * @f: as i said
+ *
+ * Return: no return for void function.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/acp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+commit="0x0F-function_pointers"
+
+git add .
+git commit -m "$commit"
+git push
